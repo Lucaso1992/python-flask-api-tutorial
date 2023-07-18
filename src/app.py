4ba4ca0c498f51ac47c5cdcab25744d4c6d936b3
Lucas Solari <128732015+Lucaso1992@users.noreply.github.com>
2023-07-18 09:20:42 +0000

    finished

--- src/app.py
+++ src/app.py
@@ -10,11 +10,18 @@ def get_data():
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    request_body = request.data
-    print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+    request_body = request.get_json(force=True)
+    todos.append(request_body)
 
+    todo_list = jsonify(todos)
+    return todo_list
 
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    del todos[position]
+
+    todo_list = jsonify(todos)
+    return todo_list 
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
